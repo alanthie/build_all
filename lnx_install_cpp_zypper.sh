d954f34174fc1811e6717f81158de05e9c0c1155
Alain Lanthier <alanthier001@hotmail.ca>
2025-02-19 00:14:16 -0500

    Update lnx_install_cpp_zypper.sh

--- lnx_install_cpp_zypper.sh
+++ lnx_install_cpp_zypper.sh
@@ -14,7 +14,7 @@
 # https://github.com/alanthie/cryptochat2
 #
 # SET FOLDER where git repository are downloaded
-# SET FOLDER where packages (binaries) are output
+# SET FOLDERPKG where packages (binaries) are output
 # SET BUILDTYPE
 # 
 # Update your Linux first
